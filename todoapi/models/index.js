7470698e26f23029792956a32304e6c66c465b1a
Yurockkk <r00631026@ntu.edu.tw>
2018-02-12 17:21:49 -0500

    Update mongoose credential

--- todoapi/models/index.js
+++ todoapi/models/index.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/todo-api');
+// mongoose.connect('mongodb://localhost/todo-api');
+mongoose.connect('mongodb://heroku_9djcdx20:ndj9ugvuv98fc7fgbhjcu42ic5@ds233228.mlab.com:33228/heroku_9djcdx20')
 
 mongoose.Promise = Promise;
 
