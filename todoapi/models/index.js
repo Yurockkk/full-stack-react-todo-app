915a4a582499775e7548edc8faa3dedbcd55670c
Yurockkk <r00631026@ntu.edu.tw>
2018-02-12 17:26:50 -0500

    Update mongoose url

--- todoapi/models/index.js
+++ todoapi/models/index.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
-// mongoose.connect('mongodb://localhost/todo-api');
-mongoose.connect('mongodb://heroku_9djcdx20:ndj9ugvuv98fc7fgbhjcu42ic5@ds233228.mlab.com:33228/heroku_9djcdx20')
+
+mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.Promise = Promise;
 
