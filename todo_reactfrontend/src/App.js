aecfd5d1aae55552bc0d458066f8061dfe4f5322
Yurockkk <r00631026@ntu.edu.tw>
2018-02-14 00:36:06 -0500

    Add bootstrap

--- todo_reactfrontend/src/App.js
+++ todo_reactfrontend/src/App.js
@@ -5,8 +5,8 @@ import TodoList from './TodoList';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <TodoList />
+      <div className="App container">
+        <TodoList className="col-10" />
       </div>
     );
   }
